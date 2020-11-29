015a0e7e2c6c6ba33d8ec62c50a404c2b4d6c3c0
Rodrigo Kreutz <8869678+rkreutz@users.noreply.github.com>
2020-11-29 00:46:41 -0300

    Migrating CredentialProvider to SCA

--- CredentialProvider/CredentialProviderViewController.swift
+++ CredentialProvider/CredentialProviderViewController.swift
@@ -8,87 +8,124 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
 
     private var cancellableStore: Set<AnyCancellable> = []
 
+    lazy var reducer = PasswordGeneratorView.Reducer.combine(
+        PasswordGeneratorView.sharedReducer,
+        PasswordGeneratorView.Reducer { [extensionContext] state, action, _ -> Effect<PasswordGeneratorView.Action, Never> in
+
+            switch action {
+
+            case let .updatedPasswordState(.updateFlow(.generated(password))) where state.configurationState.passwordType == .domainBased:
+                extensionContext.completeRequest(
+                    withSelectedCredential: ASPasswordCredential(
+                        user: state.configurationState.domainState.username,
+                        password: password
+                    ),
+                    completionHandler: nil
+                )
+                return .none
+
+            case let .updatedPasswordState(.updateFlow(.generated(password))) where state.configurationState.passwordType == .serviceBased:
+                extensionContext.completeRequest(
+                    withSelectedCredential: ASPasswordCredential(
+                        user: state.configurationState.serviceState.service,
+                        password: password
+                    ),
+                    completionHandler: nil
+                )
+                return .none
+
+            default:
+                return .none
+            }
+        }
+    )
+
+    lazy var store = Store<PasswordGeneratorView.State, PasswordGeneratorView.Action>(
+        initialState: .init(
+            configurationState: .init(
+                passwordType: .domainBased,
+                domainState: .init(
+                    username: "",
+                    domain: "",
+                    seed: .init(
+                        title: Strings.PasswordGeneratorView.seed.formatted(),
+                        count: 1,
+                        bounds: 1 ... 999
+                    ),
+                    isValid: false
+                ),
+                serviceState: .init(
+                    service: "",
+                    isValid: false
+                ),
+                isValid: false
+            ),
+            lengthState: .init(
+                lengthState: .init(
+                    title: Strings.PasswordGeneratorView.passwordLength.formatted(),
+                    count: 8,
+                    bounds: 4 ... 32
+                )
+            ),
+            charactersState: .init(
+                digitsState: .init(
+                    toggleTitle: Strings.PasswordGeneratorView.decimalCharacters.formatted(),
+                    isToggled: true,
+                    counterState: .init(
+                        title: Strings.PasswordGeneratorView.numberOfCharacters.formatted(),
+                        count: 1,
+                        bounds: 1 ... 8
+                    )
+                ),
+                symbolsState: .init(
+                    toggleTitle: Strings.PasswordGeneratorView.symbolsCharacters.formatted(),
+                    isToggled: false,
+                    counterState: .init(
+                        title: Strings.PasswordGeneratorView.numberOfCharacters.formatted(),
+                        count: 0,
+                        bounds: 1 ... 8
+                    )
+                ),
+                lowercaseState: .init(
+                    toggleTitle: Strings.PasswordGeneratorView.lowercasedCharacters.formatted(),
+                    isToggled: true,
+                    counterState: .init(
+                        title: Strings.PasswordGeneratorView.numberOfCharacters.formatted(),
+                        count: 1,
+                        bounds: 1 ... 8
+                    )
+                ),
+                uppercaseState: .init(
+                    toggleTitle: Strings.PasswordGeneratorView.uppercasedCharacters.formatted(),
+                    isToggled: true,
+                    counterState: .init(
+                        title: Strings.PasswordGeneratorView.numberOfCharacters.formatted(),
+                        count: 1,
+                        bounds: 1 ... 8
+                    )
+                ),
+                isValid: true
+            ),
+            passwordState: .init(
+                flow: .invalid,
+                copyableState: .init(content: "")
+            )
+        ),
+        reducer: reducer,
+        environment: PasswordGeneratorView.Environment.live()
+    )
+
+    var viewStore: ViewStore<PasswordGeneratorView.State, PasswordGeneratorView.Action> {
+
+        ViewStore(store)
+    }
+
     @IBSegueAction
     private func addSwiftUI(_ coder: NSCoder) -> UIViewController? {
 
         UIHostingController(
             coder: coder,
-            rootView: PasswordGeneratorView(
-                store: Store<PasswordGeneratorView.State, PasswordGeneratorView.Action>.init(
-                    initialState: .init(
-                        configurationState: .init(
-                            passwordType: .domainBased,
-                            domainState: .init(
-                                username: "",
-                                domain: "",
-                                seed: .init(
-                                    title: Strings.PasswordGeneratorView.seed.formatted(),
-                                    count: 1,
-                                    bounds: 1 ... 999
-                                ),
-                                isValid: false
-                            ),
-                            serviceState: .init(
-                                service: "",
-                                isValid: false
-                            ),
-                            isValid: false
-                        ),
-                        lengthState: .init(
-                            lengthState: .init(
-                                title: Strings.PasswordGeneratorView.passwordLength.formatted(),
-                                count: 8,
-                                bounds: 4 ... 32
-                            )
-                        ),
-                        charactersState: .init(
-                            digitsState: .init(
-                                toggleTitle: Strings.PasswordGeneratorView.decimalCharacters.formatted(),
-                                isToggled: true,
-                                counterState: .init(
-                                    title: Strings.PasswordGeneratorView.numberOfCharacters.formatted(),
-                                    count: 1,
-                                    bounds: 1 ... 8
-                                )
-                            ),
-                            symbolsState: .init(
-                                toggleTitle: Strings.PasswordGeneratorView.symbolsCharacters.formatted(),
-                                isToggled: false,
-                                counterState: .init(
-                                    title: Strings.PasswordGeneratorView.numberOfCharacters.formatted(),
-                                    count: 0,
-                                    bounds: 1 ... 8
-                                )
-                            ),
-                            lowercaseState: .init(
-                                toggleTitle: Strings.PasswordGeneratorView.lowercasedCharacters.formatted(),
-                                isToggled: true,
-                                counterState: .init(
-                                    title: Strings.PasswordGeneratorView.numberOfCharacters.formatted(),
-                                    count: 1,
-                                    bounds: 1 ... 8
-                                )
-                            ),
-                            uppercaseState: .init(
-                                toggleTitle: Strings.PasswordGeneratorView.uppercasedCharacters.formatted(),
-                                isToggled: true,
-                                counterState: .init(
-                                    title: Strings.PasswordGeneratorView.numberOfCharacters.formatted(),
-                                    count: 1,
-                                    bounds: 1 ... 8
-                                )
-                            ),
-                            isValid: true
-                        ),
-                        passwordState: .init(
-                            flow: .invalid,
-                            copyableState: .init(content: "")
-                        )
-                    ),
-                    reducer: PasswordGeneratorView.sharedReducer,
-                    environment: PasswordGeneratorView.Environment.live()
-                )
-            )
+            rootView: PasswordGeneratorView(store: store)
                 .frame(maxWidth: 450)
                 .accentColor(.accentColor)
                 .background(
@@ -99,36 +136,13 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
         )
     }
 
-    override func viewDidLoad() {
-
-        super.viewDidLoad()
-
-//        viewState.$state
-//            .compactMap { state -> String? in
-//
-//                guard case let .generated(password) = state.passwordState else { return nil }
-//                return password
-//            }
-//            .sink { [extensionContext, viewState] password in
-//
-//                extensionContext.completeRequest(
-//                    withSelectedCredential: ASPasswordCredential(
-//                        user: viewState.state.configurationState.username,
-//                        password: password
-//                    ),
-//                    completionHandler: nil
-//                )
-//            }
-//            .store(in: &cancellableStore)
-    }
-
     override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier]) {
 
         let host = serviceIdentifiers
             .first { $0.type == .URL }
             .flatMap { URL(string: $0.identifier)?.host }
 
-//        viewState.state.configurationState.domain = host ?? ""
+        viewStore.send(.updatedConfigurationState(.updateDomain(.updateDomain(host ?? ""))))
     }
 
     @IBAction private func cancel(_ sender: AnyObject?) {
